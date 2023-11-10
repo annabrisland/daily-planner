590fd39b010e44828a7e34f1ae823b63ab0d0644
annabrisland <84166964+annabrisland@users.noreply.github.com>
2023-11-10 13:16:35 -0800

    Creates schedule time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 var currentTimeEL = $("#time-display");
 var currentDayEl = $("#date-display");
+var scheduleEl = $("#schedule-display");
 
-// FUnction to get current time & date
+// Function to get current time & date
 function displayTime() {
     var nowTime = dayjs().format("HH:mm:ss");
     var nowDate = dayjs().format("DD MMM YYYY");
@@ -10,3 +11,24 @@ function displayTime() {
 }
 // Call and refresh time every second
 setInterval(displayTime, 1000);
+
+// Function to add schedule layout
+function displaySchedule() {
+    for (var i = 9; i <= 17; i++) {
+        if (i > 12) {
+            var convertedTime = i - 12 + "PM";
+        } else {
+            var convertedTime = i + "AM";
+        };
+
+        var timeBlockEl = $("<div>");
+        var timeslot = $("<div>").text(convertedTime).addClass("hour");
+        var timeBlock = $("<input>").addClass("time-block");
+        var addButton = $("<button>").text("Add").addClass("saveBtn");
+        timeBlockEl.append(timeslot, timeBlock, addButton);
+        scheduleEl.addClass("schedule-block").append(timeBlockEl);
+    }
+   
+}
+
+displaySchedule();
